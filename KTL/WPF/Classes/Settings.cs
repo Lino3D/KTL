7140303c365888851114781c633ff04413392c18
Lino3D <kamiskijakub@gmail.com>
2016-12-15 14:38:05 +0100

    Settings Class

---
+++ KTL/WPF/Classes/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Classes
+{
+    public class Settings
+    {
+        public int AllColors { get; set; }
+        public int RoundColors { get; set; }
+        public int BoardLength { get; set; }
+        public int SeriesLength { get; set; }
+
+        public Settings(int a, int b, int c, int d)
+        {
+            AllColors = a;
+            RoundColors = b;
+            BoardLength = c;
+            SeriesLength = d;
+        }
+
+
+    }
+}
