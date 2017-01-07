fb3bd9b55497c204a542782ea187f9169d329e04
Lino3D <kamiskijakub@gmail.com>
2017-01-07 18:31:28 +0100

    IPlayer1

---
+++ KTL/WPF/obj/IPlayer1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Interfaces
+{
+    public interface IPlayer1
+    {
+        int SelectBoardCell();
+    }
+}
